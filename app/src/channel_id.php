3f9e6f51909cb9a3eef1917f6de32f8d8891a93c
Chikara Hamada <norg.hamada@gmail.com>
2024-02-13 17:51:36 +0900

    入力フォームから値受け取り

--- app/src/channel_id.php
+++ app/src/channel_id.php
@@ -1,9 +1,11 @@
 <?php
-$youtubeurl = "https://www.youtube.com/@studytimer";
+//var_dump($_POST['channel_id']);
+//exit;
+$youtubeurl = $_POST['channel_id'];
 $home = file_get_contents($youtubeurl);
 $url1 = explode('/channel/',$home);
 $url2 = explode('"',$url1[1]);
-var_dump($url2);
+//var_dump($url2);
 $channel_id = $url2[0];
 
 echo $channel_id;
