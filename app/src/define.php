48b52fd280787c4e3e900e99e8795531fc9d2d67
Chikara Hamada <norg.hamada@gmail.com>
2024-02-13 17:06:11 +0900

    定数追加

--- app/src/define.php
+++ app/src/define.php
@@ -1,6 +1,18 @@
 <?php
+//youtbeapikey
 define('YOUTUBE_API_KEY1', 'AIzaSyBjUEjjLg9RJFD7_lujNFVgV6z2L7hNwKE');
 define('YOUTUBE_API_KEY2', 'AIzaSyCpVmU3bMIeae7e8VlG-SFyLfDnml2y_lA');
 define('YOUTUBE_API_KEY3', 'AIzaSyC0bGbwRk3mPBOIiP-h2Po8rT_XakxeCCs');
 define('YOUTUBE_API_KEY4', 'AIzaSyD4s45kVBQhgJ7BhtIlYWHa9SacvlEa8-Y');
+define('YOUTUBE_API_KEY5', 'AIzaSyCOXUQA-fF7euc5_2YN-haIqz-IFjJtSGI');
+define('YOUTUBE_API_KEY6', 'AIzaSyBizlgPM3vGmeQhWN5Wqm_h09fEgAbGMek');
 // APIキー　定数で設置 回数制限で次のAPIキーを使えるようにしたい。
+
+
+define('HOST', 'run-php-db-api');
+define('DBNAME', 'api_app');
+define('USERNAME', 'root');
+define('PASSWORD', 'root');
+
+
+
