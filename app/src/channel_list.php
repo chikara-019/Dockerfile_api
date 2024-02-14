148f593a4431ae4482258194190bf38657a6a4be
Chikara Hamada <norg.hamada@gmail.com>
2024-02-14 11:34:04 +0900

    api定数を設定しないと上手くいかない

--- app/src/channel_list.php
+++ app/src/channel_list.php
@@ -8,18 +8,20 @@ try{
     $pdo = new PDO("mysql:host=" . HOST . ";dbname=" . DBNAME, USERNAME, PASSWORD);
     $sql = "SELECT channel_id FROM channel_id";
     $stmt = $pdo->query($sql);
-    // チャンネルリスト
+    
     $channel_list = $stmt->fetchAll(PDO::FETCH_NUM);
 
-    // 全動画を格納する配列
+    
     $all_videos = [];
+    $api_keys = [YOUTUBE_API_KEY1, YOUTUBE_API_KEY2, YOUTUBE_API_KEY3, YOUTUBE_API_KEY4, YOUTUBE_API_KEY5, YOUTUBE_API_KEY6];
+    //foreachで回数制限がかかったら次のapikeyになるようにしたい
 
-    // 各チャンネルから動画を取得して統合
-    foreach ($channel_list as $channel) {
-        $response = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?key=' . YOUTUBE_API_KEY6 . '&channelId=' . $channel[0] . '&part=snippet&order=date&maxResults=1&type=video'), true);
+    
+    foreach($channel_list as $channel) {
+        $response = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?key=' . YOUTUBE_API_KEY7 . '&channelId=' . $channel[0] . '&part=snippet&order=date&maxResults=1&type=video'), true);
             
-        if ($response && isset($response['items'])) {
-               foreach ($response['items'] as $item) {
+        if($response && isset($response['items'])){
+               foreach ($response['items'] as $item){
                 $all_videos[] = $item;
             }
         }
@@ -32,7 +34,7 @@ usort($all_videos, function($a, $b) {
     return strtotime($b['snippet']['publishedAt']) - strtotime($a['snippet']['publishedAt']);
 });
 
-// 動画を表示
+
 foreach ($all_videos as $video) {
 
     echo '<a href="https://www.youtube.com/watch?v=' . $video['id']['videoId'] . '">' . $video['snippet']['title'] . '</a><br>';
